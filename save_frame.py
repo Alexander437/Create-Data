f2baf2e9398051b47e448041f70a699f6f7b0b6d
Alexander Berezin <berezin-ayu@mail.ru>
2021-03-14 16:31:15 +0300

    add augmentation

--- save_frame.py
+++ save_frame.py
@@ -1,5 +1,5 @@
-import cv2
 import time
+import cv2
 
 class SaveFrame():
 
@@ -9,7 +9,7 @@ class SaveFrame():
 
     def __call__(self, label, delay=20):
         cap = cv2.VideoCapture(self.camera_port)
-        count = 1
+        count = 0
         count_frame = 0
 
         while cap.isOpened():
@@ -17,13 +17,20 @@ class SaveFrame():
             if not ret:
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
-            path = '/home/pi/Create-Data/images/' + label + '_' + str(count) + '.jpeg'
+            path_base = '/home/pi/Create-Data/images/' + label + '_'
             if (self.mode%2 == 1):
                 count_frame += 1
                 if (count_frame%delay == 0):
+                    path = path_base + str(count) + '.jpeg'
                     cv2.imwrite(path, frame)
+                    path = path_base + str(count+1) + '.jpeg'
+                    cv2.imwrite(path, self.augmentation(frame, 10))
+                    path = path_base + str(count+2) + '.jpeg'
+                    cv2.imwrite(path, self.augmentation(frame, 15))
+                    path = path_base + str(count+3) + '.jpeg'
+                    cv2.imwrite(path, self.augmentation(frame, -15))
                     print('writing img: ', path)
-                    count += 1
+                    count += 4
 
             cv2.imshow('frame', frame)
             key = cv2.waitKey(1)
@@ -37,3 +44,12 @@ class SaveFrame():
 
         cap.release()
         cv2.destroyAllWindows()
+
+    def augmentation(self, img, grad):
+        rows,cols,cannels = img.shape
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),grad,1)
+
+        path = 'images/img.jpeg'
+        dst = cv2.warpAffine(img,M,(cols,rows))
+        return(cv2.resize(dst[50:430, 85:540, :], (640,480)))
+
